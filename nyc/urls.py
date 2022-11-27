3f588b2342f53454e881f2a11fc43bc1959db312
daniel jackson <jackdee0077@gmail.com>
2022-11-27 15:48:34 -0500

    updated url

--- nyc/urls.py
+++ nyc/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', CityView.as_view(), name='city'),
     path('<str:borough>', BoroughView.as_view(), name='borough'),
     path('<str:borough>/<str:activity>', ActivityView.as_view(), name='activity'),
-    path('<str:borough>/<str:activity>/<str:venue>', VenueView.as_view(), name='venue')
+    path('<str:borough>/<str:activity>/<str:venue>', VenueView.as_view(), name='venue'),
 ]
